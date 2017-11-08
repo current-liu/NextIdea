35938e4e101abf7b9466193d424db4a4133e0061
liuchao <liuchao_mail@qq.com>
2017-11-07 23:34:06 -0800

    anaysis 1.1 each reset_index will add a column

--- anaysis/sales/service.py
+++ anaysis/sales/service.py
@@ -68,13 +68,13 @@ def get_product_added_into_cart(request):
     """Function: in specific period,product added into cart"""
     time_begin = request.GET.get('t1')
     time_end = request.GET.get('t2')
-    sku = request.GET.get('sku')
+    product_id = request.GET.get('product_id')
     # time_begin = '2010-09-17 00:00:00'
     # time_end = '2021-01-01 00:00:00'
     # time_begin = datetime.datetime.strptime('2017-09-10 00:00:00', '%Y-%m-%d %H:%M:%S')
     # time_end = datetime.datetime.strptime('2017-09-15 23:59:59', '%Y-%m-%d %H:%M:%S')
 
-    sale_orders_info = pd.merge(sale_orders, sale_orders_products, how='outer', left_on='id', right_on='order_id')
+    sale_orders_info = pd.merge(sale_orders, sale_orders_products, left_on='id', right_on='order_id')
 
     if (time_begin is None) | (time_end is None):
         product_count_in_cart = cart
@@ -87,12 +87,12 @@ def get_product_added_into_cart(request):
         sale_orders_info.set_index(['created_at'], inplace=True)
         product_count_in_order = sale_orders_info[time_begin:time_end]
 
-    if sku is None:
-        product_count_in_cart = product_count_in_cart[['sku_id', 'quantity']].groupby(by='sku_id').sum()
-        product_count_in_order = product_count_in_order[['sku', 'quantity']].groupby(by='sku').sum()
+    if product_id is None:
+        product_count_in_cart = product_count_in_cart[['product_id', 'quantity']].groupby(by='product_id').sum()
+        product_count_in_order = product_count_in_order[['product_id', 'quantity']].groupby(by='product_id').sum()
     else:
-        product_count_in_cart = product_count_in_cart[product_count_in_cart['sku_id'] == sku]
-        product_count_in_order = product_count_in_order[product_count_in_order['sku' == sku]]
+        product_count_in_cart = product_count_in_cart[product_count_in_cart['product_id'] == product_id]
+        product_count_in_order = product_count_in_order[product_count_in_order['product_id' == product_id]]
     print product_count_in_cart
     print product_count_in_order
 
@@ -100,7 +100,7 @@ def get_product_added_into_cart(request):
     product_count_added_in_cart = product_count_in_cart.add(product_count_in_order, fill_value=0)
     print product_count_added_in_cart
 
-    product_count_added_in_cart.index.name = 'sku'
+    product_count_added_in_cart.index.name = 'product_id'
     r = product_count_added_in_cart.reset_index().to_dict('index')
     return JsonResponse(r, safe=False)
 
@@ -167,7 +167,7 @@ def get_product_sale_info(request):
 
 
 def anaysis_sale(request):
-    sku = request.GET.get('sku')
+    product_id = request.GET.get('product_id')
     try:
         manufacturer_id = int(request.GET.get('manufacturer_id'))
     except:
@@ -192,8 +192,8 @@ def anaysis_sale(request):
         ['product_id', 'sku', 'name', 'category_id', 'is_active', 'created_by', 'created_at', 'price',
          'manufacturer_id', ]]
 
-    if sku is not None:
-        catalog_product_info = catalog_product_info[catalog_product_info['sku'] == sku]
+    if product_id is not None:
+        catalog_product_info = catalog_product_info[catalog_product_info['product_id'] == product_id]
         pass
     if manufacturer_id is not None:
         catalog_product_info = catalog_product_info[catalog_product_info['manufacturer_id'] == manufacturer_id]
@@ -211,20 +211,22 @@ def anaysis_sale(request):
         catalog_product_info = catalog_product_info[catalog_product_info['category_id'] == category_id]
         pass
 
-    sku_list = catalog_product_info['sku']
-    product_added_into_cart = anaysis_product_added_into_cart(time_begin, time_end, sku_list)
-    product_sale_info = anaysis_product_sale_info(time_begin, time_end, sku_list)
+    product_id_list = catalog_product_info['product_id']
+    product_added_into_cart = anaysis_product_added_into_cart(time_begin, time_end, product_id_list)
+    product_sale_info = anaysis_product_sale_info(time_begin, time_end, product_id_list)
     res = pd.merge(catalog_product_info, pd.merge(product_added_into_cart, product_sale_info))
 
     decimals = pd.Series([2, 2], index=['paid_amount', 'ordered_to_paid'])
     return JsonResponse(res.round(decimals).to_dict('index'), safe=False)
 
 
-def anaysis_product_added_into_cart(time_begin, time_end, sku_list):
+def anaysis_product_added_into_cart(time_begin, time_end, product_id_list):
     """Function: in specific period,product added into cart"""
 
-    sale_orders_products_1 = sale_orders_products[sale_orders_products['sku'].isin(sku_list)]
-    cart_1 = cart[cart['sku_id'].isin(sku_list)]
+    sale_orders_products_1 = sale_orders_products[sale_orders_products['product_id'].isin(product_id_list)]
+    cart_1 = cart[cart['product_id'].isin(product_id_list)]
+    print "cart_1"
+    print cart_1
 
     sale_orders_info = pd.merge(sale_orders, sale_orders_products_1, left_on='id', right_on='order_id')
 
@@ -232,51 +234,52 @@ def anaysis_product_added_into_cart(time_begin, time_end, sku_list):
         product_count_in_cart = cart_1
         product_count_in_order = sale_orders_info
     else:
-        cart_1 = cart.set_index(['created_at'], drop=False)
-        product_count_in_cart = cart_1[time_begin:time_end]
+        cart_1 = cart_1.set_index(['created_at'], drop=False)
+        product_count_in_cart = cart_1[time_begin:time_end].reset_index()
 
         # sale_orders_info['created_at'] = sale_orders_info['created_at'].astype('datetime64[ns]')
         sale_orders_info.set_index(['created_at'], inplace=True)
-        product_count_in_order = sale_orders_info[time_begin:time_end]
+        product_count_in_order = sale_orders_info[time_begin:time_end].reset_index()
 
-    # print product_count_in_cart
+    print "product_count_in_cart"
+    print product_count_in_cart
     # print product_count_in_order
 
     # result
-    product_count_in_cart_mini = product_count_in_cart[['sku_id', 'quantity']].reset_index().groupby(by='sku_id').sum()
-    product_count_in_order_mini = product_count_in_order[['sku', 'quantity']].reset_index().groupby(by='sku').sum()
+    product_count_in_cart_mini = product_count_in_cart[['product_id', 'quantity']].groupby(by='product_id').sum()
+    product_count_in_order_mini = product_count_in_order[['product_id', 'quantity']].groupby(by='product_id').sum()
     product_count_added_in_cart = product_count_in_cart_mini.add(product_count_in_order_mini, fill_value=0)
 
     product_count_added_in_cart.reset_index(inplace=True)
-    product_count_added_in_cart.columns = ['sku', "cart_quantity"]
+    product_count_added_in_cart.columns = ['product_id', "cart_quantity"]
     # print product_count_added_in_cart
     return product_count_added_in_cart
 
 
-def anaysis_product_sale_info(time_begin, time_end, sku_list):
+def anaysis_product_sale_info(time_begin, time_end, product_id_list):
     """Function: in specific period,product turned into pay status"""
     sale_orders_period = sale_orders.set_index(['created_at'])
     sale_orders_period = sale_orders_period[time_begin:time_end]
 
-    sale_orders_products_1 = sale_orders_products[sale_orders_products['sku'].isin(sku_list)]
+    sale_orders_products_1 = sale_orders_products[sale_orders_products['product_id'].isin(product_id_list)]
     sale_orders_products_1['amount'] = sale_orders_products_1['price'] * sale_orders_products_1['quantity']
 
     sale_orders_info = pd.merge(sale_orders_period, sale_orders_products_1, left_on='id', right_on='order_id')
 
     # result: in pay
 
-    product_count_in_ordered = sale_orders_info[
-        (sale_orders_info['status'] == 'Paid') | (sale_orders_info['status'] == 'Paid Processing') | (
-            sale_orders_info['status'] == 'Shipping') | (
-            sale_orders_info['status'] == 'Issue')].sort_values('sku')
+    # product_count_in_ordered = sale_orders_info[
+    #     (sale_orders_info['status'] == 'Paid') | (sale_orders_info['status'] == 'Paid Processing') | (
+    #         sale_orders_info['status'] == 'Shipping') | (
+    #         sale_orders_info['status'] == 'Issue')].sort_values('sku')
 
     status_list = ['Paid', 'Paid Processing', 'Shipping', 'Issue']
     product_count_in_ordered = sale_orders_info[sale_orders_info['status'].isin(status_list)]
     print product_count_in_ordered
-    product_count_in_ordered = product_count_in_ordered[['sku', 'quantity']].groupby(by='sku').sum()
+    product_count_in_ordered = product_count_in_ordered[['product_id', 'quantity']].groupby(by='product_id').sum()
     print product_count_in_ordered
     product_count_in_ordered.reset_index(inplace=True)
-    product_count_in_ordered.columns = ['sku', 'ordered_quantity']
+    product_count_in_ordered.columns = ['product_id', 'ordered_quantity']
     # dict_product_count_in_paying = product_count_in_ordered.reset_index().to_dict('index')
     # print product_count_in_ordered
     # return JsonResponse(dict_product_count_in_paying, safe=False)
@@ -287,7 +290,7 @@ def anaysis_product_sale_info(time_begin, time_end, sku_list):
     # time_paid_end = '2017-09-23 23:59:59'
 
     product_count_in_paid = sale_orders_info[(sale_orders_info['payment_status'] == 'Completed')]
-    product_sales_count = pd.pivot_table(product_count_in_paid, values=['quantity', 'amount'], index='sku',
+    product_sales_count = pd.pivot_table(product_count_in_paid, values=['quantity', 'amount'], index='product_id',
                                          aggfunc=np.sum)
     product_sales_count.columns = ['paid_amount', 'paid_quantity']
     product_sales_count.reset_index(inplace=True)
